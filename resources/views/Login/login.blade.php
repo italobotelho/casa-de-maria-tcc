bf564226c09685ad8854dbd0c7e97901c943f33e
italobotelho <fragabotelhoitalo@gmail.com>
2024-08-17 14:32:30 -0300

    criaçcao de arquivos sass e e node.js

--- resources/views/Login/login.blade.php
+++ resources/views/Login/login.blade.php
@@ -8,7 +8,6 @@
     <!-- Link para o arquivo CSS de estilos personalizados da página de login -->
     <link rel="stylesheet" type="text/css" href="{{ asset('css/login.css')}}" />
 
-    <!-- Link para o CSS do Bootstrap a partir de uma CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
