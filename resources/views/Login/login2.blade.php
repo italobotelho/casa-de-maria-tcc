ba04d5df9b7ef7954cc5e7fd3134c2dce55de2ff
Italo Botelho <fragabotelhoitalo@gmail.com>
2024-09-04 09:02:12 -0300

    pendencia tela login v2

--- resources/views/Login/login2.blade.php
+++ resources/views/Login/login2.blade.php
@@ -16,42 +16,50 @@
 
     <section class="vh-100">  
         
-        <!----------------------- Main Container -------------------------->
+    
 
-        <div class="container d-flex justify-content-center align-items-center min-vh-100">
-
-            <!----------------------- Login Container -------------------------->
-            <div class="row border rounded-5 p-3 bg-white shadow box-area">
-
-                <!--------------------------- Left Box ----------------------------->
-                <div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box" style="background: #103cbe;">
-                    <div class="featured-image mb-3">
-                     <img src="img/logo.png" class="img-fluid" style="width: ;">
-                    </div>  
-                </div>
-
-                <!-------------------- ------ Right Box ---------------------------->
-        
-                <div class="col-md-6 right-box">
-                    <div class="row align-items-center">
-                        <div class="header-text mb-4">
-                            <h1 style="color: #795127;">SEU LOGIN</h1>
-                        </div>
-                        <div data-mdb-input-init class="form-outline mb-4 d-grid gap-2 col-10 mx-auto">
-                            <input type="input" name="user" class="form-control form-control-lg rounded-pill shadow-sm border border-0" style="background-color: rgb(255, 255, 255, 0.5);"  id="inputEmailUser" aria-describedby="userHelp" placeholder="USUÁRIO">
-                        </div>
       
-                        <div data-mdb-input-init class="form-outline mb-4 d-grid gap-2 col-10 mx-auto">
-                            <input type="password" name="senha" class="form-control form-control-lg rounded-pill shadow-sm border border-0" style="background-color: rgb(255, 255, 255, 0.5);" id="inputPassword" placeholder="SENHA">
-                        </div>
-                        <div class=" mb-4 d-grid gap-2 col-6 mx-auto">
-                            <button data-mdb-button-init data-mdb-ripple-init class="btn btn-lg rounded-pill shadow-sm" style="background-color: rgb(138, 99, 58); color: white;" type="submit">ENTRAR</button>
-                          </div>
-                    </div>
-                </div> 
+     
+          <!----------------------- Main Container -------------------------->
+      
+           <div class="container d-flex justify-content-center align-items-center min-vh-100">
+      
+          <!----------------------- Login Container -------------------------->
+      
+             <div class="row rounded-5 p-3 shadow box-area" style="border-radius: 1rem; background-color: #E5D5C0;">
+      
+          <!--------------------------- Left Box ----------------------------->
+      
+             <div class="col-md-6 rounded-4 flex-column left-box">
+                 <div class="featured-image mb-3">
+                  <img src="img/logo.png" class="img-fluid" style="background-color: rgb(201, 156, 101, 0.5); border-radius: 1rem; width: 250px;">
+                 </div> 
+             </div> 
+      
+          <!-------------------- ------ Right Box ---------------------------->
+              
+             <div class="col-md-6 d-flex right-box">
+                <div class="row align-items-center">
+                      <div class="header-text mb-4">
+                           <h1 style="color: #795127;">SEU LOGIN</h1>
+                      </div>
+                      <div class="input-group mb-3">
+                          <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Email address">
+                      </div>
+                      <div class="input-group mb-1">
+                          <input type="password" class="form-control form-control-lg bg-light fs-6" placeholder="Password">
+                      </div>
+                      <div class="input-group mb-5 d-flex justify-content-between">
+                      </div>
+                      <div class="input-group mb-3">
+                          <button class="btn btn-lg btn-primary w-100 fs-6">Login</button>
+                      </div>
+                </div>
+             </div> 
+      
             </div>
-        </div>
-      </section>
+          </div>
+      
 
     <!-- Link para o JavaScript do Bootstrap a partir de uma CDN -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
