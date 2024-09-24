dc5230461f86b5acf9092a14c11495e992da888e
italobotelho <fragabotelhoitalo@gmail.com>
2024-09-23 21:05:26 -0300

    arrumando responsividade e usando mais bootstrap

--- resources/views/Layout/layout.blade.php
+++ resources/views/Layout/layout.blade.php
@@ -6,13 +6,24 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Casa de Maria</title>
+    @yield('head')
 </head>
 <body>
     <header>
-        
+        <!--Navbar-->
+        <nav class="navbar navbar-expand-lg fixed-top" style="background-color: #795127;">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="img/logo.png" alt="Logo" width="30" height="40" class="d-inline-block align-text-top">
+                </a>
+                <a class="navbar-brand" href="#">
+                    <img src="img/conf.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
+                </a>
+            </div>
+        </nav>
     </header>
     <main>
-      
+        @yield('main')
     </main>
     <footer>
 
