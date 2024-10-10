6a1145307104c5dddd6c239170b198d7b21384ac
Italo Botelho <fragabotelhoitalo@gmail.com>
2024-10-10 11:42:12 -0300

    mudança do web route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\LoginController;
 use App\Http\Controllers\MedicoController;
 use App\Http\Controllers\PersonController;
 use App\Http\Controllers\ClinicaController;
@@ -9,44 +8,18 @@ use App\Http\Controllers\ConvenioController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-
+// Rotas auth
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/', function () {
     return view('auth/login');
 });
 
-Route::get('/layout', function () {
-    return view('Layout/layout');
-});
-
-// Rotas da Tela Menu
-
-// Rotas da Tela Menu
-
-Route::get('/agenda', function () {
-    return view('Menu/agenda');
-});
-
-Route::get('/consulta', function () {
-    return view('Menu/consulta');
-});
-
-// Rota para a lista de pacientes
-Route::get('/pacientes', [PersonController::class, 'index'])->name('pacientes.index');
-
 // Rota para o profissional (medico)
 Route::get('/profissional', [MedicoController::class, 'index'])->name('medicos.index');
 
-// Rota para o formulário de cadastro de paciente
-Route::get('/form_paciente', function () {
-    return view('Cadastros/form_paciente');
-});
-
-// Rota para armazenar um novo paciente
-Route::post('/form_paciente', [PersonController::class, 'store'])->name('paciente.store');
-
 // Rota para listar convênios
 Route::get('/convenio', [PersonController::class, 'ListarConvenio'])->name('convenio.listar');
-
 
 // Rota para o formulário de cadastro de médico
 Route::get('/form_medico', function () {
@@ -55,13 +28,8 @@ Route::get('/form_medico', function () {
 
 // Rota para armazenar um novo médico
 Route::post('/form_medico', [MedicoController::class, 'store'])->name('medico.store');
-Route::get('menu/pacientes', [PersonController::class, 'index'])->name('pacientes.index');
 
-// Rotas da Tela Configurações Gerais
 
-Route::get('/gerais', 'App\Http\Controllers\ClinicaController@index')->name('clinica.index');
-Route::post('/gerais', 'App\Http\Controllers\ClinicaController@store')->name('clinica.store');
-Route::patch('/gerais', 'App\Http\Controllers\ClinicaController@update')->name('clinica.update');
 
 // Rotas da Tela Pacientes
 
@@ -73,7 +41,21 @@ Route::post('/convenios', 'PersonController@ListarConvenio');
 Route::get('Menu/pacientes', [PersonController::class, 'index'])->name('pacientes.index');
 Route::get('/pacientes', [PersonController::class, 'index'])->name('pacientes.index');
 
-Auth::routes();
+// Rota para o formulário de cadastro de paciente
+Route::get('/form_paciente', function () {
+    return view('Cadastros/form_paciente');
+});
+
+// Rota para armazenar um novo paciente
+Route::post('/form_paciente', [PersonController::class, 'store'])->name('paciente.store');
+
+
+// Rotas da Tela Configurações Gerais
+
+Route::get('/gerais', 'App\Http\Controllers\ClinicaController@index')->name('clinica.index');
+Route::post('/gerais', 'App\Http\Controllers\ClinicaController@store')->name('clinica.store');
+Route::patch('/gerais', 'App\Http\Controllers\ClinicaController@update')->name('clinica.update');
+
 // Rotas da Tela Configurações Procedimentos
 Route::get('/procedimentos', [ProcedimentoController::class, 'index'])->name('procedimentos.index');
 Route::get('/procedimentos/create', [ProcedimentoController::class, 'create'])->name('procedimentos.create');
@@ -90,4 +72,3 @@ Route::post('/convenios', [ConvenioController::class, 'store'])->name('convenios
 Route::get('/convenios/{pk_id_conv}/edit', [ConvenioController::class, 'edit'])->name('convenios.edit');
 Route::patch('/convenios/{pk_id_conv}', [ConvenioController::class, 'update'])->name('convenios.update');
 Route::delete('/convenios/{pk_id_conv}', [ConvenioController::class, 'destroy'])->name('convenios.destroy');
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
