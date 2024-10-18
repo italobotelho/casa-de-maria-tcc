6c9e2e5aacf4e30ce08533eca441087bad3cf8b4
italobotelho <fragabotelhoitalo@gmail.com>
2024-10-18 20:36:10 -0300

    fix: labels id

--- resources/views/Agenda/modal-calendar.blade.php
+++ resources/views/Agenda/modal-calendar.blade.php
@@ -19,73 +19,66 @@
                   <label for="title" class="form-label">Paciente</label>
                   <input type="text" class="form-control" name="title" id="title" placeholder="Informe o nome, código ou data de nascimento...">
                   <input type="hidden" name="id">
-                </div>
+              </div>
 
-                <div class="col-md-6">
-                  <label for="" class="form-label">Profissional</label>
-                  <select class="form-select" aria-label="Default select example">
-                    <option selected>Selecione o Profissional</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
+              <div class="col-md-6">
+                  <label for="professional" class="form-label">Profissional</label>
+                  <select class="form-select" id="professional" aria-label="Default select example">
+                      <option selected>Selecione o Profissional</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
                   </select>
-                </div>
+              </div>
 
-                {{-- <div class="form-check form-switch col col-ms-auto text-center">
-                  <label class="form-check-label d-block" for="flexSwitchCheckDefault">Bloqueado</label>
-                  <div class="d-flex justify-content-center">
-                    <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckBlock" data-bs-target="#exampleModalToggle" data-bs-toggle="modalBlock">
-                  </div>
-                </div> --}}
-                
-                <div class="col-md-6">
-                  <label for="" class="form-label">Procedimento</label>
-                  <select class="form-select" aria-label="Default select example">
-                    <option selected>Open this select menu</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
+              <div class="col-md-6">
+                  <label for="procedure" class="form-label">Procedimento</label>
+                  <select class="form-select" id="procedure" aria-label="Default select example">
+                      <option selected>Open this select menu</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
                   </select>
-                </div>
+              </div>
 
-                <div class="col-md-2">
+              <div class="col-md-2">
                   <label for="start" class="form-label">Hora Inicial</label>
                   <input type="time" class="form-control" name="start" id="start">
                   <div class="input-group-append" id="startButtons" style="display: none;">
                       <button type="button" class="btn btn-secondary btn-sm" id="increaseStartTime">↑</button>
                       <button type="button" class="btn btn-secondary btn-sm" id="decreaseStartTime">↓</button>
                   </div>
-                </div>
-                
-                <div class="col-md-2">
-                    <label for="end" class="form-label">Hora Final</label>
-                    <input type="time" class="form-control" name="end" id="end">
-                    <div class="input-group-append" id="endButtons" style="display: none;">
-                        <button type="button" class="btn btn-secondary btn-sm" id="increaseEndTime">↑</button>
-                        <button type="button" class="btn btn-secondary btn-sm" id="decreaseEndTime">↓</button>
-                    </div>
-                </div>
+              </div>
 
-                <div class="form-check form-switch col col-ms-auto text-center">
-                  <label class="form-check-label d-block" for="flexSwitchCheckDefault">Repetição</label>
-                  <div class="d-flex justify-content-center">
-                    <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckRepeate" data-bs-target="#exampleModalToggle" data-bs-toggle="modalRepeate">
+              <div class="col-md-2">
+                  <label for="end" class="form-label">Hora Final</label>
+                  <input type="time" class="form-control" name="end" id="end">
+                  <div class="input-group-append" id="endButtons" style="display: none;">
+                      <button type="button" class="btn btn-secondary btn-sm" id="increaseEndTime">↑</button>
+                      <button type="button" class="btn btn-secondary btn-sm" id="decreaseEndTime">↓</button>
                   </div>
-                </div>
+              </div>
 
-                <div class="col-md-6">
-                  <label for="" class="form-label">Convênio do Paciente</label>
-                  <select class="form-select" aria-label="Default select example">
-                    <option selected>Open this select menu</option>
+              <div class="form-check form-switch col col-ms-auto text-center">
+                  <label class="form-check-label d-block" for="flexSwitchCheckRepeate">Repetição</label>
+                  <div class="d-flex justify-content-center">
+                      <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckRepeate" data-bs-target="#exampleModalToggle" data-bs-toggle="modalRepeate">
+                  </div>
+              </div>
+
+              <div class="col-md-6">
+                  <label for="convenio" class="form-label">Convênio do Paciente</label>
+                  <select class="form-select" id="convenio" aria-label="Default select example">
+                      <option selected>Open this select menu</option>
                     <option value="1">One</option>
                     <option value="2">Two</option>
                     <option value="3">Three</option>
                   </select>
                 </div>
-                
+
                 <div class="col-md-6">
-                  <label for="" class="form-label">Status</label>
-                  <select class="form-select" aria-label="Default select example">
+                  <label for="status" class="form-label">Status</label>
+                  <select class="form-select" id="status" aria-label="Default select example">
                     <option selected>Open this select menu</option>
                     <option value="1">One</option>
                     <option value="2">Two</option>
