3a2a166ad240b59d210a31e6632261e91671f282
ana gomes <anaclaravjma2006@gmail.com>
2024-10-22 19:34:47 -0300

    css tela busca

--- resources/views/Menu/pacientes.blade.php
+++ resources/views/Menu/pacientes.blade.php
@@ -18,12 +18,20 @@
         border:none;
         padding:15px;
         }
+    
+        .modal-content {
+        padding: 20px;
+        background-color: white;
+        border-radius: 5px;
+        border: 1px solid #ddd;
+        }
+
     </style>
 </head>
 <body>
     <br><br>
-    <a class="cad" href="form_paciente">CADASTRAR PACIENTE</a>
     
+    <a class="cad" href="form_paciente">CADASTRAR PACIENTE</a>
 
     <div class="container">
         <div class="row justify-content-center">
@@ -32,54 +40,57 @@
             <div class="col-lg-8">
                 <div class="card shadow">
                     <div class="card-header text-white d-flex justify-content-between">
-                       <h1 class="">Pacientes</h1>
+                       <h1>Pacientes</h1>
                     </div>
-                    <div class="card-body p-4">
-                        <table class="table table-striped">
-                        <thead>
-                            <tr>
-                            <th scope="col">codigo</th>
-                            <th scope="col">Nome</th>
-                            <th scope="col">Data nascimento</th>
-                            <th scope="col">Convenio</th>
-                            <th scope="col">Telefone</th>
-                            <th scope="col">CPF</th>
-                            <th scope="col">Cidade</th>
 
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @forelse($pacientes as $paciente)
-                                <tr>
-                                <th scope="row">{{$paciente->pk_cod_paci}}</th>
-                                <td>{{$paciente->nome_paci}}</td>
-                                <td>{{ \Carbon\Carbon::parse($paciente->data_nasci_paci)->format('d/m/Y') }}</td>
-                                <td>{{$paciente->convenio->nome_conv}}</td>
-                                <td>{{$paciente->telefone_paci}}</td>
-                                <td>{{$paciente->cpf_paci}}</td>
-                                <td>{{$paciente->nome_cidade}}</td>
-                                {{-- <td>
-                                    <div class="d-flex gap-1">
-                                    <a href="/pacientes/{{$paciente->id}}" class="btn btn-secondary">Editar</a>
-                                    <form action="/pacientes/{{$paciente->id}}" method="POST">
-                                        @method('delete')
-                                        @csrf
-                                        <button class="btn btn-danger">
-                                            Excluir
-                                        </button>
-                                    </form>
-                                    </div>
-                                </td> --}}
-                                </tr>
-                            @empty
-                                <tr>
-                                    <td colspan="8" class="text-center fs-4">
-                                        Nenhum paciente cadastrado
-                                    </td>
-                                </tr>
-                            @endforelse
-                        </tbody>
-                        </table>
+                        <div class="modal-content">
+                            <div class="card-body p-4">
+                                <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                    <th scope="col">codigo</th>
+                                    <th scope="col">Nome</th>
+                                    <th scope="col">Data nascimento</th>
+                                    <th scope="col">Convenio</th>
+                                    <th scope="col">Telefone</th>
+                                    <th scope="col">CPF</th>
+                                    <th scope="col">Cidade</th>
+
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @forelse($pacientes as $paciente)
+                                        <tr>
+                                        <th scope="row">{{$paciente->pk_cod_paci}}</th>
+                                        <td>{{$paciente->nome_paci}}</td>
+                                        <td>{{ \Carbon\Carbon::parse($paciente->data_nasci_paci)->format('d/m/Y') }}</td>
+                                        <td>{{$paciente->convenio->nome_conv}}</td>
+                                        <td>{{$paciente->telefone_paci}}</td>
+                                        <td>{{$paciente->cpf_paci}}</td>
+                                        <td>{{$paciente->nome_cidade}}</td>
+                                        {{-- <td>
+                                            <div class="d-flex gap-1">
+                                            <a href="/pacientes/{{$paciente->id}}" class="btn btn-secondary">Editar</a>
+                                            <form action="/pacientes/{{$paciente->id}}" method="POST">
+                                                @method('delete')
+                                                @csrf
+                                                <button class="btn btn-danger">
+                                                    Excluir
+                                                </button>
+                                            </form>
+                                            </div>
+                                        </td> --}}
+                                        </tr>
+                                    @empty
+                                        <tr>
+                                            <td colspan="8" class="text-center fs-4">
+                                                Nenhum paciente cadastrado
+                                            </td>
+                                        </tr>
+                                    @endforelse
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div><!-- fim da col -->
