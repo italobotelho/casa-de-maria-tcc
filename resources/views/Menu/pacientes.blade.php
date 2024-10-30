68dc2ef75ce3aad9c607586c49cf98f323f515fa
provence <anaprovence30@gmail.com>
2024-10-30 19:56:03 -0300

    tentando colocar na mainnnnn

--- resources/views/Menu/pacientes.blade.php
+++ resources/views/Menu/pacientes.blade.php
@@ -1,238 +1,82 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Seus Pacientes</title>
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<?php
+
+use App\Http\Controllers\MedicoController;
+use App\Http\Controllers\PersonController;
+use App\Http\Controllers\ClinicaController;
+use App\Http\Controllers\ProcedimentoController;
+use App\Http\Controllers\ConvenioController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+// Rotas auth
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return view('auth/login');
+});
+
+// Rota para o profissional (medico)
+Route::get('/profissional', [MedicoController::class, 'index'])->name('medicos.index');
+
+// Rota para listar convênios
+Route::get('/convenio', [PersonController::class, 'ListarConvenio'])->name('convenio.listar');
+
+// Rota para o formulário de cadastro de médico
+Route::get('/form_medico', function () {
+    return view('Cadastros/form_medico');
+});
+
+// Rota para armazenar um novo médico
+Route::post('/form_medico', [MedicoController::class, 'store'])->name('medico.store');
 
 
-<style>
-    #convenio-suggestions {
-    position: absolute;
-    background-color: #f9f9f9;
-    border: 1px solid #ccc;
-    padding: 10px;
-    width: 200px;
-    z-index: 1000;
-}
 
-#convenio-suggestions li {
-    padding: 5px;
-    cursor: pointer;
-}
+// Rotas da Tela Pacientes
 
-#convenio-suggestions li:hover {
-    background-color: #ccc;
-}
-</style>
+// Rota para atualizar os dados de um paciente
+Route::post('/update-paciente', [PersonController::class, 'update'])->name('paciente.update');
+Route::post('/convenios', 'PersonController@ListarConvenio');
 
 
-</head>
-<body>
-   {{-- testanddoo --}}
- 
-   <a href="form_paciente">CADASTRAR PACIENTE</a>
+Route::get('Menu/pacientes', [PersonController::class, 'index'])->name('pacientes.index');
+Route::get('/pacientes', [PersonController::class, 'index'])->name('pacientes.index');
 
-   <div class="container">
-       <div class="row justify-content-center">
-           <div class="col-lg-8 mt-4"></div>
-           <div class="col-lg-8">
-               <div class="card shadow">
-                   <div class="card-header bg-success text-white d-flex justify-content-between">
-                       <h1 class="fw-bold">Pacientes</h1>
-                   </div>
-                   <div class="card-body p-4">
-                       <table class="table table-striped">
-                           <thead>
-                               <tr>
-                                   <th scope="col">Código</th>
-                                   <th scope="col">Nome</th>
-                                   <th scope="col">Data Nascimento</th>
-                                   <th scope="col">Convênio</th>
-                                   <th scope="col">Carteira</th>
-                                   <th scope="col">Telefone</th>
-                                   <th scope="col">CPF</th>
-                                   <th scope="col">Cidade</th>
-                                   <th scope="col">Nome Responsável</th>
-                                   <th scope="col">CPF Responsável</th>
-                               </tr>
-                           </thead>
-                           <tbody>
-                               @forelse($pacientes as $paciente)
-                                   <tr>
-                                       <th scope="row">{{ $paciente->pk_cod_paci }}</th>
-                                        <td>
-                                            <a href="#" class="nome-paciente" 
-                                            data-id="{{ $paciente->pk_cod_paci }}" 
-                                            data-nome="{{ $paciente->nome_paci }}" 
-                                            data-data-nasci="{{ \Carbon\Carbon::parse($paciente->data_nasci_paci)->format('d/m/Y') }}" 
-                                            data-convenio="{{ $paciente->convenio->nome_conv }}" 
-                                            data-telefone="{{ $paciente->telefone_paci }}" 
-                                            data-cpf="{{ $paciente->cpf_paci }}" 
-                                            data-cidade="{{ $paciente->nome_cidade }}" 
-                                            data-responsavel="{{ $paciente->responsavel_paci }}" 
-                                            data-cpf-responsavel="{{ $paciente->cpf_responsavel_paci }}" 
-                                            data-carteira-convenio="{{ $paciente->carteira_convenio_paci }}">
-                                            {{ $paciente->nome_paci }}
-                                        </a>
-                                           <button class="btn btn-warning btn-sm editar-paciente" 
-                                                   data-id="{{ $paciente->pk_cod_paci }}" 
-                                                   data-nome="{{ $paciente->nome_paci }}" 
-                                                   data-email="{{ $paciente->email_paci }}" 
-                                                   data-data-nasci="{{ \Carbon\Carbon::parse($paciente->data_nasci_paci)->format('Y-m-d') }}" 
-                                                   data-convenio-id="{{ $paciente->fk_convenio_paci }}"
-                                                   data-telefone="{{ $paciente->telefone_paci }}" 
-                                                   data-cpf="{{ $paciente->cpf_paci }}" 
-                                                   data-cidade="{{ $paciente->nome_cidade }}" 
-                                                   data-responsavel="{{ $paciente->responsavel_paci }}"        
-                                                   data-cpf-responsavel="{{ $paciente->cpf_responsavel_paci }}"
-                                                   data-carteira-convenio="{{ $paciente->carteira_convenio_paci }}">
-                                               Editar
-                                           </button>
-                                       </td>
-                                       <td>{{ \Carbon\Carbon::parse($paciente->data_nasci_paci)->format('d/m/Y') }}</td>
-                                       <td>{{ $paciente->convenio->nome_conv }}</td>
-                                       <td>{{ $paciente->carteira_convenio_paci }}</td>
-                                       <td>{{ $paciente->telefone_paci }}</td>
-                                       <td>{{ $paciente->cpf_paci }}</td>
-                                       <td>{{ $paciente->nome_cidade }}</td>
-                                       <td>{{ $paciente->responsavel_paci }}</td>
-                                       <td>{{ $paciente->cpf_responsavel_paci }}</td>
-                                   </tr>
-                               @empty
-                                   <tr>
-                                       <td colspan="10" class="text-center fs-4">
-                                           Nenhum paciente cadastrado
-                                       </td>
-                                   </tr>
-                               @endforelse
-                           </tbody>
-                       </table>
-                   </div>
-               </div>
-           </div><!-- fim da col -->
-       </div><!-- fim da row -->
-   </div> <!-- fim do container -->
+// Rota para o formulário de cadastro de paciente
+Route::get('/form_paciente', function () {
+    return view('Cadastros/form_paciente');
+});
 
-   <a href="menu">VOLTAR</a>
-
-   <div id="pacienteModal" class="modal fade" tabindex="-1" role="dialog">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Informações do Paciente</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <p><strong>Nome:</strong> <span id="nome-paciente"></span></p>
-                <p><strong>Data de Nascimento:</strong> <span id="data-nascimento"></span></p>
-                <p><strong>Convênio:</strong><span id="convenio"></span></p>
-                <p><strong>Carteira do Convênio:</strong><span id="carteira_convenio"></span></p>
-                <p><strong>Telefone:</strong> <span id="telefone"></span></p>
-                <p><strong>CPF:</strong><span id="cpf"></span></p>
-                <p><strong>Cidade:</strong><span id="cidade"></span></p>
-                <p><strong>Responavel:</strong><span id="responsavel"></span></p>
-                <p><strong>CPF do Responsavel:</strong><span id="cpf-responsavel"></span></p>
-            </div>
-        </div>
-    </div>
-</div>
-
-<p><strong>Nome:</strong>
-
-  {{-- modal edicao --}}
-
-  <div class="modal fade" id="editarPacienteModal" tabindex="-1" role="dialog" aria-labelledby="editarPacienteModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h3 class="modal-title" id="editarPacienteModalLabel">Editar Paciente</h3>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <form action="{{ route('paciente.update') }}" id="formEditarPaciente">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <div class="modal-body">
-                    <input maxlength="54" type="text"  id="editar-id" name="id">
-                    <div class="form-group">
-                        <label for="editar-nome">Nome</label>
-                        <input type="text" class="form-control" id="editar-nome" name="nome" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="editar-email">E-mail</label>
-                        <input type="email" maxlength="255" class="form-control" id="editar-email" name="email" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="editar-data-nasci">Data de Nascimento</label>
-                        <input type="date" class="form-control" id="editar-data-nasci" name="data_nasci" required>
-                    </div>
+// Rota para armazenar um novo paciente
+Route::post('/form_paciente', [PersonController::class, 'store'])->name('paciente.store');
 
 
-                        <div class="form-group">
-                            <label for="fk_convenio_paci">Convênio</label>
-                            <select name="fk_convenio_paci" id="fk_convenio_paci">
-                                @foreach($convenios as $convenio)
-                                    <option value="{{ $convenio->pk_id_conv }}" 
-                                        @if(isset($paciente) && $convenio->pk_id_conv === $paciente->fk_convenio_paci) selected @endif>
-                                        {{ $convenio->nome_conv }}
-                                    </option>
-                                @endforeach
-                            </select>
-                           </div>
+// Rotas da Tela Configurações Gerais
 
-                           <div id="carteira-convenio-field" style="display: none;">
-                            <label for="editar-carteira-convenio">Carteira do Convênio:</label>
-                            <input type="text" maxlength="255" id="editar-carteira-convenio" name="carteira_convenio">
-                          </div>
+Route::get('/gerais', 'App\Http\Controllers\ClinicaController@index')->name('clinica.index');
+Route::post('/gerais', 'App\Http\Controllers\ClinicaController@store')->name('clinica.store');
+Route::patch('/gerais', 'App\Http\Controllers\ClinicaController@update')->name('clinica.update');
 
-                        
+// Rotas da Tela Configurações Procedimentos
+Route::get('/procedimentos', [ProcedimentoController::class, 'index'])->name('procedimentos.index');
+Route::get('/procedimentos/create', [ProcedimentoController::class, 'create'])->name('procedimentos.create');
+Route::get('/procedimentos/{pk_cod_proc}', [ProcedimentoController::class, 'show'])->name('procedimentos.show');
+Route::post('/procedimentos', [ProcedimentoController::class, 'store'])->name('procedimentos.store');
 
-                    <div class="form-group">
-                        <label for="editar-telefone">Telefone</label>
-                        <input type="text" 
-                        maxlength="15"
-                        class="form-control" id="editar-telefone" name="telefone" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="editar-cpf">CPF</label>
-                        <input type="text" 
-                        maxlength="14"
-                        class="form-control" id="editar-cpf" name="cpf" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="editar-cidade">Cidade</label>
-                        <input type="text" 
-                        maxlength="100" class="form-control" id="editar-cidade" name="cidade" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="editar-responsavel">Nome do Responsável</label>
-                        <input type="text" class="form-control" id="editar-responsavel" name="responsavel">
-                    </div>
-                    <div class="form-group">
-                        <label for="editar-cpf-responsavel">CPF do Responsável</label>
-                        <input type="text" class="form-control" id="editar-cpf-responsavel" name="cpf_responsavel">
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-                    <button type="submit" class="btn btn-primary">Salvar alterações</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
 
-            <script src="js/paciente.js"></script>
+//Rotas da Tela Configurações Convenios
 
-  
+Route::get('/convenios', [ConvenioController::class, 'index'])->name('convenios.index');
+Route::get('/convenios/create', [ConvenioController::class, 'create'])->name('convenios.create');
+Route::post('/convenios', [ConvenioController::class, 'store'])->name('convenios.store');
 
-</body>
-</html>
+Route::get('/convenios/{pk_id_conv}/edit', [ConvenioController::class, 'edit'])->name('convenios.edit');
+Route::patch('/convenios/{pk_id_conv}', [ConvenioController::class, 'update'])->name('convenios.update');
+Route::delete('/convenios/{pk_id_conv}', [ConvenioController::class, 'destroy'])->name('convenios.destroy');
+
+
+
+// Rota para o formulário de edição de médico
+Route::get('/medicos', [MedicoController::class, 'edit'])->name('medico.edit');
+
+// Rota para atualizar os dados de um médico
+Route::post('/update-medico', [MedicoController::class, 'update'])->name('medico.update');
