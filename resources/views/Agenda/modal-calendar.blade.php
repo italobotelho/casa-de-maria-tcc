beec48e91e87ccf0bf5557224f2258825c13728d
provence <anaprovence30@gmail.com>
2024-11-09 21:04:15 -0300

    modal de sucesso sumindo apos 750 milésimos segundos

--- resources/views/Agenda/modal-calendar.blade.php
+++ resources/views/Agenda/modal-calendar.blade.php
@@ -21,14 +21,15 @@
             opacity: 0.5;
         }
 
-            /* Deixa todos os labels em negrito */
+        /* Deixa todos os labels em negrito */
         .form-label {
-            font-weight: bold; /* Aplica negrito aos labels */
+            font-weight: bold;
+            /* Aplica negrito aos labels */
         }
-      
-   /* ARRUMAR O ESCURECIMENTO DO MODAL PRINCIPAL QUANDO O MODAL DE EXCLUSAO É ABERTOOOOO */
+
+        /* ARRUMAR O ESCURECIMENTO DO MODAL PRINCIPAL QUANDO O MODAL DE EXCLUSAO É ABERTOOOOO */
     </style>
-    
+
 
     <!-- Modal para agendamento de eventos -->
     <div class="modal fade" id="modalCalendar" tabindex="-1" aria-labelledby="titleModal" aria-hidden="true">
@@ -45,11 +46,11 @@
                         <div id="message"></div> <!-- Área para exibir mensagens (ex: erros ou confirmações) -->
                         <div id="successMessage" class="alert alert-success" style="display: none;"></div> <!-- Mensagem de sucesso -->
 
-                        
+
                         <form id="formEvent" class="row g-3"> <!-- Formulário com classe para estilo de grade -->
                             <input type="hidden" name="id" id="id"> <!-- Campo oculto para ID -->
                             <input type="hidden" name="color" id="color" value=""> <!-- Campo oculto para cor -->
-                        
+
                             <!-- Campo para paciente -->
                             <div class="col-12">
                                 <label for="paciente" class="form-label">Paciente</label>
@@ -57,7 +58,7 @@
                                 <input type="text" class="form-control" name="paciente" id="paciente" placeholder="Informe o nome" oninput="buscarPacientes(this.value)" autocomplete="off">
                                 <div id="pacienteSuggestions" class="list-group" style="display: none; position: absolute; z-index: 1000;"></div> <!-- Sugestões para pacientes -->
                             </div>
-                        
+
                             <!-- Campo para médico -->
                             <div class="col-12">
                                 <label for="medico" class="form-label">Profissional</label>
@@ -65,21 +66,21 @@
                                 <input type="text" class="form-control" name="medico_nome" id="medico_nome" placeholder="Informe o medico" oninput="buscarMedico(this.value)" autocomplete="off">
                                 <div id="medicoSuggestions" class="list-group" style="display: none; position: absolute; z-index: 1000;"></div> <!-- Sugestões para médicos -->
                             </div>
-                        
+
                             <!-- Campo para procedimento -->
                             <div class="col-12">
                                 <label for="procedimento_id" class="form-label">Procedimento</label>
                                 <select class="form-select" id="procedimento_id" name="procedimento_id" aria-label="Default select example">
                                     <option value="">Selecione um Procedimento</option> <!-- A opção padrão deve ter um valor vazio -->
                                     @foreach($procedimentos as $procedimento)
-                                        <option value="{{ $procedimento->pk_cod_proc }}">{{ $procedimento->nome_proc }}</option>
+                                    <option value="{{ $procedimento->pk_cod_proc }}">{{ $procedimento->nome_proc }}</option>
                                     @endforeach
                                 </select>
                             </div>
-                        
+
                             <!-- Campo para data do evento -->
                             <input type="hidden" class="form-control" name="eventDate" id="eventDate">
-                        
+
                             <!-- Campos para hora inicial e final -->
                             <div class="col-12 d-flex align-items-end"> <!-- Flex container para alinhar os campos -->
                                 <div class="me-2" style="flex: 2;"> <!-- Coluna para a hora inicial -->
@@ -91,7 +92,7 @@
                                     <input type="time" class="form-control" name="end" id="end">
                                 </div>
                             </div>
-                        
+
                             <!-- Campo para repetição -->
                             {{-- <div class="col-12">
                                 <label class="form-check-label d-block" for="flexSwitchCheckRepeate">Repetição</label>
@@ -99,7 +100,7 @@
                                     <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckRepeate"> <!-- Switch para repetição -->
                                 </div>
                             </div> --}}
-                        
+
                             <!-- Campo para convênio do paciente -->
                             <div class="col-12">
                                 <label for="convenio_id" class="form-label">Convênio</label>
@@ -117,61 +118,84 @@
         </div>
     </div>
 
-<!-- Modal de Confirmação de Exclusão -->
-<div class="modal fade" id="confirmDeleteModal" tabindex="-1" aria-labelledby="confirmDeleteModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="confirmDeleteModalLabel">Confirmar Exclusão</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                Você tem certeza que deseja excluir este evento?
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                <button type="button" class="btn btn-danger" id="confirmDeleteButton">Excluir</button>
+    <!-- Modal de Confirmação de Exclusão -->
+    <div class="modal fade" id="confirmDeleteModal" tabindex="-1" aria-labelledby="confirmDeleteModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="confirmDeleteModalLabel">Confirmar Exclusão</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Você tem certeza que deseja excluir este evento?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                    <button type="button" class="btn btn-danger" id="confirmDeleteButton">Excluir</button>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
-<!-- Modal de Sucesso -->
-<div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="successModalLabel">Sucesso</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <p>Consulta cadastrada com sucesso!</p> <!-- Mensagem de sucesso -->
-            </div>
+    <!-- Modal de Sucesso -->
+    <div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="successModalLabel">Sucesso</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>Consulta cadastrada com sucesso!</p> <!-- Mensagem de sucesso -->
+                </div>
                 <p id="successMessageContent"></p> <!-- Conteúdo da mensagem de sucesso -->
             </div>
         </div>
     </div>
-</div>
+    </div>
 
-<!-- Modal de Sucesso para Alteração -->
-<div class="modal fade" id="successAlterationModal" tabindex="-1" aria-labelledby="successAlterationModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="successAlterationModalLabel">Sucesso</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <p>Alteração realizada!</p> <!-- Mensagem de sucesso -->
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+    <!-- Modal de Sucesso para Alteração -->
+    <div class="modal fade" id="successAlterationModal" tabindex="-1" aria-labelledby="successAlterationModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="successAlterationModalLabel">Sucesso</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>Alteração realizada!</p> <!-- Mensagem de sucesso -->
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                </div>
             </div>
         </div>
     </div>
-</div>
+    <script>
+    // Function to close modal after 2 seconds
+    const successModal = document.getElementById('successModal');
+    const successAlterationModal = document.getElementById('successAlterationModal');
+
+    // Listen for modal showing event
+    successModal.addEventListener('shown.bs.modal', function () {
+        setTimeout(function () {
+            $('#successModal').modal('hide'); // Close the modal
+        }, 750); // 2000ms = 2 seconds
+    });
+
+    successAlterationModal.addEventListener('shown.bs.modal', function () {
+        setTimeout(function () {
+            $('#successAlterationModal').modal('hide'); // Close the modal
+        }, 750); // 2000ms = 2 seconds
+    });
+</script>
+
 
     <script src="js/buscaPaciente.js"></script> <!-- Script para manipulação das buscas de pacientes -->
+
+
+
+    
     </body>
 
     </html>
