ec1381b1704f4b0767f4b80688036c6c62fa3369
provence <anaprovence30@gmail.com>
2024-11-11 10:27:56 -0300

    deixando as cores mais claras

--- resources/views/Agenda/modal-view-calendar.blade.php
+++ resources/views/Agenda/modal-view-calendar.blade.php
@@ -1,81 +1,87 @@
 <div class="modal fade modal-lg" id="modalViewCalendar" aria-hidden="true" aria-labelledby="modalViewCalendarLabel" tabindex="-1">
-  <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content">
-          <div class="modal-header">
-              <h1 class="modal-title fs-5" id="modalViewCalendarLabel"></h1>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            <div>
-                profile
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="modalViewCalendarLabel"></h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div>
-                <p><strong>Nome:</strong> <span id="pacienteNome"></span></p>
-                <p><strong>Telefone:</strong> <span id="pacienteTelefone"></span></p>
-                <p><strong>Convênio:</strong> <span id="pacienteConvenio"></span></p>
-                <p><strong>Médico:</strong> <span id="medicoNome"></span></p>
-                <p><strong>Hora Inicial:</strong> <span id="horaInicial"></span></p>
-                <p><strong>Hora Final:</strong> <span id="horaFinal"></span></p>
-              </div>
-              <div>
-                Últimas consultas
-              </div>
-          </div>
-          <div class="modal-footer d-flex justify-content-between">
-            <div class="dropup-center dropup">
-                <button class="btn btn-success dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Status
-                </button>
-                <ul class="dropdown-menu">
-                    <li>
-                        <a class="dropdown-item" href="#" data-color="#008000"> <!-- Cor: Verde -->
-                            <span class="dot" style="background-color: #008000;"></span> Finalizado
-                        </a>
-                    </li>
-                    <li>
-                        <a class="dropdown-item" href="#" data-color="#9D9D9B"> <!-- Cor: Cinza -->
-                            <span class="dot" style="background-color: #9D9D9B;"></span> Agendado
-                        </a>
-                    </li>
-                    <li>
-                        <a class="dropdown-item" href="#" data-color="#0000ff"> <!-- Cor: Azul -->
-                            <span class="dot" style="background-color: #0000ff;"></span> Confirmado
-                        </a>
-                    </li>
-                    <li>
-                        <a class="dropdown-item" href="#" data-color="#ffa500"> <!-- Cor: Laranja -->
-                            <span class="dot" style="background-color: #ffa500;"></span> Esperando
-                        </a>
-                    </li>
-                    <li>
-                        <a class="dropdown-item" href="#" data-color="#ff7bff"> <!-- Cor: Rosa -->
-                            <span class="dot" style="background-color: #ff7bff;"></span> Não Compareceu
-                        </a>
-                    </li>
-                    <li>
-                        <a class="dropdown-item" href="#" data-color="#ff0000"> <!-- Cor: Vermelho -->
-                            <span class="dot" style="background-color: #ff0000;"></span> Cancelado
-                        </a>
-                    </li>
-                    <li>
-                        <a class="dropdown-item" href="#" data-color="#a52a2a"> <!-- Cor: Marrom -->
-                            <span class="dot" style="background-color: #a52a2a;"></span> Reagendado
-                        </a>
-                    </li>
-                </ul>
+            <div class="modal-body">
+                <div>
+                    profile
+                </div>
+                <div>
+                    <p><strong>Nome:</strong> <span id="pacienteNome"></span></p>
+                    <p><strong>Telefone:</strong> <span id="pacienteTelefone"></span></p>
+                    <p><strong>Convênio:</strong> <span id="pacienteConvenio"></span></p>
+                    <p><strong>Médico:</strong> <span id="medicoNome"></span></p>
+                    <p><strong>Hora Inicial:</strong> <span id="horaInicial"></span></p>
+                    <p><strong>Hora Final:</strong> <span id="horaFinal"></span></p>
+                </div>
+                <div>
+                    Últimas consultas
+                </div>
             </div>
-              <button class="btn btn-primary" data-bs-target="#modalCalendar" data-bs-toggle="modal">Alterar Agendamento</button>
-          </div>
-      </div>
-  </div>
+            <div class="modal-footer d-flex justify-content-between">
+                <div class="dropup-center dropup">
+                    <button class="btn btn-success dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Status
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a class="dropdown-item" href="#" data-color="#85d17b"> <!-- Cor: Verde -->
+                                <span class="dot" style="background-color: #85d17b;"></span> Finalizado
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#" data-color="#9D9D9B"> <!-- Cor: Cinza -->
+                                <span class="dot" style="background-color: #9D9D9B;"></span> Agendado
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#" data-color="#8ea7e8"> <!-- Cor: Azul -->
+                                <span class="dot" style="background-color: #8ea7e8;"></span> Confirmado
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#" data-color="#f0b34a"> <!-- Cor: Laranja -->
+                                <span class="dot" style="background-color: #f0b34a;"></span> Esperando
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#" data-color="#f58493"> <!-- Cor: Rosa -->
+                                <span class="dot" style="background-color: #f58493;"></span> Não Compareceu
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#" data-color="#ff0000"> <!-- Cor: Vermelho -->
+                                <span class="dot" style="background-color: #ff0000;"></span> Cancelado
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#" data-color="#803c45"> <!-- Cor: Marrom -->
+                                <span class="dot" style="background-color: #803c45"></span> Reagendado
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <button class="btn btn-primary" data-bs-target="#modalCalendar" data-bs-toggle="modal">Alterar Agendamento</button>
+            </div>
+        </div>
+    </div>
 </div>
 <style>
-  .dot {
-      height: 10px; /* Tamanho do ponto */
-      width: 10px; /* Tamanho do ponto */
-      border-radius: 50%; /* Faz o ponto ser redondo */
-      display: inline-block; /* Para que o ponto fique ao lado do texto */
-      margin-right: 8px; /* Espaçamento entre o ponto e o texto */
-      vertical-align: middle; /* Alinha o ponto verticalmente ao centro do texto */
-  }
+    .dot {
+        height: 10px;
+        /* Tamanho do ponto */
+        width: 10px;
+        /* Tamanho do ponto */
+        border-radius: 50%;
+        /* Faz o ponto ser redondo */
+        display: inline-block;
+        /* Para que o ponto fique ao lado do texto */
+        margin-right: 8px;
+        /* Espaçamento entre o ponto e o texto */
+        vertical-align: middle;
+        /* Alinha o ponto verticalmente ao centro do texto */
+    }
 </style>
